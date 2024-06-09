f5d5ba74a5d87417a9940e49234afb43362e0eb0
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 00:22:06 +0200

    fetch data from api using use effect hook

--- src/VanDetail.jsx
+++ src/VanDetail.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+
 const VanDetail = () => {
-  const params = useParams()
-  const [van, setVan] = React.useState(null)
+  const params = useParams()            //use params to fetch each vans id
+  const [van, setVan] = React.useState(null)   //init state to set van info              
+  
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+        .then(res => res.json())                      //use effect to handle data fetching using params id
+        .then(data => setVan(data.vans))              // set each van page with info
+}, [params.id])
+
   return (
    <h1>Van detail page goes here</h1>   
   )
