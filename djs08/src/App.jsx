226eb8ff6f1b6feacd42898ce708cfe3f0e5fb0d
Noxolo <wendyngcobo98@gmail.com>
2024-06-09 07:44:52 +0200

    add about route

--- djs08/src/App.jsx
+++ djs08/src/App.jsx
@@ -1,4 +1,4 @@
-  
+
 //import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1>Hello world! router here</h1>
     </>
   )
 }
