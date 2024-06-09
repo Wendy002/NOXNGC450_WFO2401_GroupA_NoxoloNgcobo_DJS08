2eee959375ea2821a349f01209857a9f953b214e
Noxolo <wendyngcobo98@gmail.com>
2024-06-09 07:18:52 +0200

    add comments

--- djs08/src/main.jsx
+++ djs08/src/main.jsx
@@ -4,10 +4,16 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+
+//create browser router
+// Nest routes and the route element inside
+// insert App to render
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-
+      <Route path='/' element={App}/>
     </Routes>
 
   </BrowserRouter>,
