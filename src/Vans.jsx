bc89997fac1861bc39f255280a58e3e930dd9d86
Noxolo <wendyngcobo98@gmail.com>
2024-06-09 15:16:19 +0200

    fetch data from api

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,5 +1,17 @@
 //vans page component
+import React from "react"
+//fetch("/api/vans")` to kick off the request to get the
+// data from our fake Mirage JS server
+//use react.useEffect
+
+
 const Vans = () => {
+
+    React.useEffect(()=>{
+        fetch("api/vans")
+        .then(res=> res.json())
+        .then(data => console.log(data))
+    },[])
   return (
     <h1>Vans page goes here 🚐</h1>
   )
