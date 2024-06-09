6fcf33295626db46e8dcfacff6e16eb68f2bfa04
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 00:18:01 +0200

    initiaize params and usestate hooks

--- src/VanDetail.jsx
+++ src/VanDetail.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 const VanDetail = () => {
+  const params = useParams()
+  const [van, setVan] = React.useState(null)
   return (
    <h1>Van detail page goes here</h1>   
   )
