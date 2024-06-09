66a8799406c2f6e9658cb3c097ce586e196992ff
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 00:27:04 +0200

    add details for each van

--- src/VanDetail.jsx
+++ src/VanDetail.jsx
@@ -11,8 +11,21 @@ const VanDetail = () => {
         .then(data => setVan(data.vans))              // set each van page with info
 }, [params.id])
 
+  // check if there is a van then displays or else show....loading..
+  // @returns image , type,price, description and action button to rent van
   return (
-   <h1>Van detail page goes here</h1>   
+    <div className="van-detail-container">
+    {van ? (
+        <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <h2>{van.name}</h2>
+            <p className="van-price"><span>${van.price}</span>/day</p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+        </div>
+    ) : <h2>Loading...</h2>}
+</div>   
   )
 }
 
