58867b963e30e76bc8905aefeee410d89e7f23b6
Noxolo <wendyngcobo98@gmail.com>
2024-06-09 13:59:32 +0200

    create and link route to  vans page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./Home.jsx"
 import About from "./About.jsx"
+import Vans from "./Vans.jsx"
 
 
 //create browser router
@@ -16,11 +17,13 @@ function App() {
           <Link to="/">#VANLIFE</Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
       </header>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>      
+      <Route path='/about' element={<About/>}/>   
+      <Route path="/vans" element={<Vans />} />   
     </Routes>
 
   </BrowserRouter>
