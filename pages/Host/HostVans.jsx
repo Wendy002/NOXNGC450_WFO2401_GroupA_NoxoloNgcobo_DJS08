cb7cbd1129fc7039ae79d82c04841d4910a6cfde
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 14:26:54 +0200

    add useEffect hook to handle side effects

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom"
 import 
 const HostVans = () => {
   const [vans, setVans] = React.useState([]) // init state
+
+  React.useEffect(() => {                  //handle side effects
+    fetch("/api/host/vans")
+        .then(res => res.json())
+        .then(data => setVans(data.vans))
+}, [])
+
  
 
   return (
