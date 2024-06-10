b896d439ed7ca8a0170ec1f08c99b79993bc3056
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 15:09:54 +0200

    add van info

--- pages/Host/HostVansDetail.jsx
+++ pages/Host/HostVansDetail.jsx
@@ -2,16 +2,27 @@ import React from "react"
 import { useParams } from "react-router-dom"
 
 const HostVansDetail = () => {
-    const [currentVan, setCurrentVan] = React.useState(null)   
-    
+    const [currentVan, setCurrentVan] = React.useState(null) 
+    const { id } = useParams()  
+
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setCurrentVan(data.vans))
     }, []) 
 
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
+
   return (
-    <div>HostVansDetail</div>
+    <div>
+        <img src={currentVan.imageUrl} width={150} />
+        <h2>{currentVan.name}</h2>
+        <p>{currentVan.price}</p>
+        <p>{currentVan.type}</p>
+    </div>
   )
 }
 
