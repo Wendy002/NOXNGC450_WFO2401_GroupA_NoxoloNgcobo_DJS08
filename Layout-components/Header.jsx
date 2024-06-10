584a8490001155526cbc953c9b3fe34295a28aea
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 00:34:18 +0200

    import avatar inside the header.jsx

--- Layout-components/Header.jsx
+++ Layout-components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom"
-
+import imageUrl from "/assets/images/avatar-icon.png"
 const Header = () => {
   return (
     <header>
@@ -8,6 +8,12 @@ const Header = () => {
             <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
             <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
             <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}> Vans</NavLink>
+            <Link to="login" className="login-link">
+                <img 
+                    src={imageUrl}
+                    className="login-icon"
+                />
+            </Link>
         </nav>
     </header>
   )
