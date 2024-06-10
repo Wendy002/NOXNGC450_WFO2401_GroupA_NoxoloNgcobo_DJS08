942945e9a917e68eba7b210c5a6a0a3fa99cc921
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 13:26:36 +0200

    Update link to nav link in header component

--- Layout-components/Header.jsx
+++ Layout-components/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
         <Link to="/">#VANLIFE</Link>
         <nav>
-            <Link to="/host">Host</Link>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
+            <NavLink to="/host">Host</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/vans">Vans</NavLink>
         </nav>
     </header>
   )
