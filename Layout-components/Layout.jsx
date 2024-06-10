779f492eda09175ac4f5c3bb7f938f0be09ba999
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 09:39:03 +0200

    Import outlet and use it on the app

--- Layout-components/Layout.jsx
+++ Layout-components/Layout.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Outlet } from "react-router-dom"
 
 const Layout = () => {
   return (
-    <div>Layout Route</div>
+    <>
+      <div>Layout Route</div>
+      <Outlet />
+    </>
+    
   )
 }
 
