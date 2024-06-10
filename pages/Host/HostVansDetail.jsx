3ba2a2af0fe7a31bc114a612fd070bc00dea6e49
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 15:03:53 +0200

    create useState

--- pages/Host/HostVansDetail.jsx
+++ pages/Host/HostVansDetail.jsx
@@ -1,6 +1,9 @@
-
+import React from "react"
+import { useParams } from "react-router-dom"
 
 const HostVansDetail = () => {
+    const [currentVan, setCurrentVan] = React.useState(null)   
+    
   return (
     <div>HostVansDetail</div>
   )
