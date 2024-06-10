cb4370c7c14b3d7bf17f8c0d1e2497bef8128361
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 16:11:23 +0200

    add the final nav bar

--- pages/Host/HostVansDetail.jsx
+++ pages/Host/HostVansDetail.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 import { useParams } from "react-router-dom"
-import { Link } from "react-router-dom"
+import { Link , NavLink, Outlet} from "react-router-dom"
+
 const HostVansDetail = () => {
     const [currentVan, setCurrentVan] = React.useState(null) 
     const { id } = useParams()  
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
 
     React.useEffect(() => {
         fetch(`/api/host/vans/${id}`)
@@ -37,6 +44,32 @@ const HostVansDetail = () => {
                     <h4>${currentVan.price}/day</h4>
                 </div>
             </div>
+            <nav className="host-van-detail-nav">
+                <NavLink
+                    to="."
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Details
+                </NavLink>
+
+                <NavLink
+                    to="pricing"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Pricing
+                </NavLink>
+
+                <NavLink
+                    to="photos"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Photos
+                </NavLink>
+
+            </nav>
+
+            <Outlet />
         </div>
     </section>
     )
