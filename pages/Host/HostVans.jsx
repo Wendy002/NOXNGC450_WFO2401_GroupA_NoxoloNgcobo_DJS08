03faa8530844291d57d5cea42d12f2ed0ce7964b
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 00:47:37 +0200

    update hostvans with new api

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,14 +1,27 @@
 
 import { Link } from "react-router-dom"
 import React from "react"
+import { getHostVans } from "../../api"
+
 
 const HostVans = () => {
   const [vans, setVans] = React.useState([]) // init state
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {                  //handle side effects
-    fetch("/api/host/vans")
-        .then(res => res.json())
-        .then(data => setVans(data.vans))
+    async function loadVans() {
+        setLoading(true)
+        try {
+            const data = await getHostVans()
+            setVans(data)
+        } catch (err) {
+            setError(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+    loadVans()
   }, [])
 
   const hostVansEls = vans.map(van => (
@@ -26,7 +39,16 @@ const HostVans = () => {
         </div>
     </Link>
     ))
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
+    }
   // return cards for each van and its detaiils
+
   return (
     <section>
         <h1 className="host-vans-title">Your listed vans</h1>      
