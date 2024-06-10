bca2885cf4dae40a919a520b4de9a56ac53dc5a5
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 09:37:24 +0200

    create a wraper router using layout component

---
+++ Layout-components/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    <div>Layout Route</div>
+  )
+}
+
+export default Layout
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./Home.jsx"
 import About from "./About.jsx"
 import Vans from "./Vans.jsx"
 import VanDetail from "./VanDetail.jsx"
+import Layout from "../Layout-components/Layout.jsx"
+
 import '../server.js'
 
 //create browser router
@@ -15,10 +17,12 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>   
-      <Route path="/vans" element={<Vans />} />
-      <Route path="/vans/:id" element={<VanDetail/>} />  
+      <Route element={<Layout/>}>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>   
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail/>} />  
+      </Route>
     </Routes>
 
   </BrowserRouter>
