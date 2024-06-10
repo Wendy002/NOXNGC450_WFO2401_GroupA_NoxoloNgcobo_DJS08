07a55364d85a14ae98adf73efd77f802b6e2e529
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 15:04:53 +0200

    create useEffect hook

--- pages/Host/HostVansDetail.jsx
+++ pages/Host/HostVansDetail.jsx
@@ -4,6 +4,12 @@ import { useParams } from "react-router-dom"
 const HostVansDetail = () => {
     const [currentVan, setCurrentVan] = React.useState(null)   
     
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, []) 
+
   return (
     <div>HostVansDetail</div>
   )
