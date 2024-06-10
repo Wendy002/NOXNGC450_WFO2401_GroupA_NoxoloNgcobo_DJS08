7c65ed1ccf184065a70d0b28283bd6dd7fa680a6
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 09:31:08 +0200

    move header to header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,6 @@ function App() {
 
   return (
     <BrowserRouter>
-    <header>
-          <Link to="/">#VANLIFE</Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-          </nav>
-    </header>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>   
