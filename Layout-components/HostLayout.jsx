8fb257177f587ed4ea2eb4b71c5eb11fed4657ba
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 13:56:45 +0200

    replace link with nav link

--- Layout-components/HostLayout.jsx
+++ Layout-components/HostLayout.jsx
@@ -1,13 +1,13 @@
-import { Link, Outlet} from "react-router-dom"
+import {Outlet, NavLink} from "react-router-dom"
 // create nav for dashboard, Income, Reviews
 
 const HostLayout = () => {
   return (
     <>
       <nav className="host-nav">
-          <Link to="/host">Dashboard</Link>
-          <Link to="/host/income">Income</Link>
-          <Link to="/host/reviews">Reviews</Link>
+          <NavLink to="/host">Dashboard</NavLink>
+          <NavLink to="/host/income">Income</NavLink>
+          <NavLink to="/host/reviews">Reviews</NavLink>
       </nav>
       <Outlet />
   </>
