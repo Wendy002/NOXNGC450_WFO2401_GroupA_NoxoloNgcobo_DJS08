51ccece905f7feded35497a66b158aee9ba1c46c
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 09:42:50 +0200

    add heander above outlet

--- Layout-components/Layout.jsx
+++ Layout-components/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Outlet } from "react-router-dom"
+import Header from './Header'
+
+
+//use outlet to control how the content of the rest of page
+//is placed as oppossed to header/ extra content 
 
 const Layout = () => {
   return (
     <>
-      <div>Layout Route</div>
+      <Header/>
       <Outlet />
     </>
     
