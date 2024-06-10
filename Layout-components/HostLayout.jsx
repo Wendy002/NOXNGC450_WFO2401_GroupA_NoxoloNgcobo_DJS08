89118e120d88d4a1de967e5f26c0ce5f426e77f2
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 14:05:43 +0200

    use end to stop matching

--- Layout-components/HostLayout.jsx
+++ Layout-components/HostLayout.jsx
@@ -1,6 +1,7 @@
 import {Outlet, NavLink} from "react-router-dom"
 // create nav for dashboard, Income, Reviews
-
+// use navlink to show active page
+// use 'end' to stop matching of active links
 const HostLayout = () => {
   const activeStyles = {
     fontWeight: "bold",
@@ -10,7 +11,7 @@ const HostLayout = () => {
   return (
     <>
       <nav className="host-nav">
-          <NavLink to="/host"  style={({ isActive }) => isActive ? activeStyles : null}>Dashboard</NavLink>
+          <NavLink to="/host" end style={({ isActive }) => isActive ? activeStyles : null}>Dashboard</NavLink>
           <NavLink to="/host/income"  style={({ isActive }) => isActive ? activeStyles : null}>Income</NavLink>
           <NavLink to="/host/reviews"  style={({ isActive }) => isActive ? activeStyles : null}>Reviews</NavLink>
       </nav>
