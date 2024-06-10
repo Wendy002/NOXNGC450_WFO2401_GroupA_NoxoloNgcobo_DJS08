72e284eda9ed0bfebe1cfd192ddd44874513a18c
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 11:12:15 +0200

    add nav for host layout

--- Layout-components/HostLayout.jsx
+++ Layout-components/HostLayout.jsx
@@ -1,8 +1,16 @@
-
+import { Link, Outlet} from "react-router-dom"
+// create nav for dashboard, Income, Reviews
 
 const HostLayout = () => {
   return (
-    <div>HostLayout</div>
+    <>
+      <nav className="host-nav">
+          <Link to="/host">Dashboard</Link>
+          <Link to="/host/income">Income</Link>
+          <Link to="/host/reviews">Reviews</Link>
+      </nav>
+      <Outlet />
+  </>
   )
 }
 
