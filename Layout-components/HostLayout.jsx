2fe97e5e7b8e6d20ad8a587fe24302649904fb3a
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 14:24:30 +0200

    add navlink for vans

--- Layout-components/HostLayout.jsx
+++ Layout-components/HostLayout.jsx
@@ -13,6 +13,7 @@ const HostLayout = () => {
       <nav className="host-nav">
           <NavLink to="/host" end style={({ isActive }) => isActive ? activeStyles : null}>Dashboard</NavLink>
           <NavLink to="/host/income"  style={({ isActive }) => isActive ? activeStyles : null}>Income</NavLink>
+          <NavLink to="/host/vans"  style={({ isActive }) => isActive ? activeStyles : null}>Vans</NavLink>
           <NavLink to="/host/reviews"  style={({ isActive }) => isActive ? activeStyles : null}>Reviews</NavLink>
       </nav>
       <Outlet />
