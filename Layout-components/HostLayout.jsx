4afb359a7aa7c25dabdd4b6af61e4d33b9e49915
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 14:01:26 +0200

    use inline styling

--- Layout-components/HostLayout.jsx
+++ Layout-components/HostLayout.jsx
@@ -5,9 +5,9 @@ const HostLayout = () => {
   return (
     <>
       <nav className="host-nav">
-          <NavLink to="/host">Dashboard</NavLink>
-          <NavLink to="/host/income">Income</NavLink>
-          <NavLink to="/host/reviews">Reviews</NavLink>
+          <NavLink to="/host"  style={({ isActive }) => isActive ? activeStyles : null}>Dashboard</NavLink>
+          <NavLink to="/host/income"  style={({ isActive }) => isActive ? activeStyles : null}>Income</NavLink>
+          <NavLink to="/host/reviews"  style={({ isActive }) => isActive ? activeStyles : null}>Reviews</NavLink>
       </nav>
       <Outlet />
   </>
