5018b3af0f80495b1f7fb05cd8edc3aff54da0dd
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 00:34:42 +0200

    create Login component

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,8 +1,10 @@
 import React from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useLocation } from "react-router-dom"
 
-export default function Login() {
+
+const Login = () => {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const location = useLocation()
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -16,28 +18,32 @@ export default function Login() {
             [name]: value
         }))
     }
-
-    return (
-        <div className="login-container">
-            <h1>Sign in to your account</h1>
-            <form onSubmit={handleSubmit} className="login-form">
-                <input
-                    name="email"
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Email address"
-                    value={loginFormData.email}
-                />
-                <input
-                    name="password"
-                    onChange={handleChange}
-                    type="password"
-                    placeholder="Password"
-                    value={loginFormData.password}
-                />
-                <button>Log in</button>
-            </form>
-        </div>
-    )
-
+  return (
+    <div className="login-container">
+        {
+            location.state?.message &&
+            <h3 className="login-first">{location.state.message}</h3>
+        }
+        <h1>Sign in to your account</h1>
+        <form onSubmit={handleSubmit} className="login-form">
+            <input
+                name="email"
+                onChange={handleChange}
+                type="email"
+                placeholder="Email address"
+                value={loginFormData.email}
+            />
+            <input
+                name="password"
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+                value={loginFormData.password}
+            />
+            <button>Log in</button>
+       </form>
+    </div>
+  )
 }
+export default Login
+
