d0f5058c0f067834a10bba8f2b4904fe382fd922
Noxolo <wendyngcobo98@gmail.com>
2024-06-10 14:28:02 +0200

    Create card for van detail

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,6 +1,7 @@
 
 import { Link } from "react-router-dom"
-import 
+import React from "react"
+
 const HostVans = () => {
   const [vans, setVans] = React.useState([]) // init state
 
@@ -8,9 +9,23 @@ const HostVans = () => {
     fetch("/api/host/vans")
         .then(res => res.json())
         .then(data => setVans(data.vans))
-}, [])
+  }, [])
 
- 
+  const hostVansEls = vans.map(van => (
+    <Link
+        to={`/host/vans/${van.id}`}
+        key={van.id}
+        className="host-van-link-wrapper"
+    >
+        <div className="host-van-single" key={van.id}>
+            <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+            <div className="host-van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}/day</p>
+            </div>
+        </div>
+    </Link>
+    ))
 
   return (
     <div>HostVans</div>
