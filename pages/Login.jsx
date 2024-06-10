80736f2273b291769ed59313f54b74fd198c2fed
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 01:32:21 +0200

    authenthication setup - sad path

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,15 +1,27 @@
 import React from "react"
-import { useNavigate, useLocation } from "react-router-dom"
+import { useNavigate, useLocation, useState } from "react-router-dom"
 import { loginUser } from "../api"
 
 const Login = () => {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
     const location = useLocation()
 
     function handleSubmit(e) {
         e.preventDefault()
+        setStatus("submitting")
         loginUser(loginFormData)
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            }) 
     }
 
     function handleChange(e) {
@@ -26,6 +38,10 @@ const Login = () => {
             <h3 className="login-first">{location.state.message}</h3>
         }
         <h1>Sign in to your account</h1>
+        {
+            error?.message &&
+            <h3 className="login-first">{error.message}</h3>
+        }
         <form onSubmit={handleSubmit} className="login-form">
             <input
                 name="email"
@@ -41,7 +57,14 @@ const Login = () => {
                 placeholder="Password"
                 value={loginFormData.password}
             />
-            <button>Log in</button>
+            <button 
+                disabled={status === "submitting"}
+            >
+                {status === "submitting" 
+                    ? "Logging in..." 
+                    : "Log in"
+                }
+            </button>
        </form>
     </div>
   )
