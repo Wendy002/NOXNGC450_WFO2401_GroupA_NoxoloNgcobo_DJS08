1483b0c366a25e370d3a86ca49e30bb234baa5db
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 08:53:47 +0200

    set up cloud firestore

--- api.js
+++ api.js
@@ -2,9 +2,34 @@
 // for the specified # of milliseconds when used w/ `await`
 // e.g. inside an async function:
 // await sleep(2000)  => pauses the function for 2 seconds before moving on
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(() => resolve(), ms))
-}
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore/lite"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-_L_D0VTI6W9tSp8bqRFBv1QbVloxIpE",
+  authDomain: "vanlife-bd625.firebaseapp.com",
+  projectId: "vanlife-bd625",
+  storageBucket: "vanlife-bd625.appspot.com",
+  messagingSenderId: "699547722606",
+  appId: "1:699547722606:web:6297141cb43a011934d77f",
+  measurementId: "G-EMRL0NF0SK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
+
+
+
 
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
