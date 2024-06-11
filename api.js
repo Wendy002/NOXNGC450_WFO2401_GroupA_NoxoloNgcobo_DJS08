18727ef772e306919d029a0bafeaaa332738e125
Noxolo <wendyngcobo98@gmail.com>
2024-06-11 09:33:36 +0200

    last update

--- api.js
+++ api.js
@@ -5,7 +5,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs, doc, query } from "firebase/firestore/lite"
+import { getFirestore, collection, getDocs,getDoc, doc, query, where, documentId } from "firebase/firestore/lite"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
